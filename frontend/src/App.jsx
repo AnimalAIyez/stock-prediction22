ce95bdc3d4693ab6de6693be489d550ee8ce815a
“K-bear” <kbearconsulting@gmail.com>
2025-04-18 07:42:39 +0900

    Front Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import './assets/css/style.css'
+import Footer from './components/Footer'
+import Main from './components/Main'
+import Header from './components/Header'
 
 
 function App() {
-
+  
 
   return (
     <>
-      
+    <Header />
+    <Main />
+    <Footer />
     </>
   )
 }
