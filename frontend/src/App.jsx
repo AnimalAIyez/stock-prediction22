6c1182fe1032589ce6076b36f9e56cbb8d160824
“K-bear” <kbearconsulting@gmail.com>
2025-04-18 12:54:12 +0900

    Reg Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import './assets/css/style.css'
 import Footer from './components/Footer'
 import Main from './components/Main'
 import Header from './components/Header'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Register from './components/Register'
+import Login from './components/Login'
 
 
 function App() {
@@ -10,9 +13,19 @@ function App() {
 
   return (
     <>
+    <BrowserRouter>
     <Header />
-    <Main />
+    <Routes>
+      <Route path='/' element={<Main />} />
+      <Route path='register/' element={<Register />} />
+      <Route path='login/' element={<Login />} />
+
+    </Routes>
     <Footer />
+    
+    </BrowserRouter>
+    
+    
     </>
   )
 }
