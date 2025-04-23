e323ab91082c2dbb41d1dbdc04c224c0bfa61520
“K-bear” <kbearconsulting@gmail.com>
2025-04-24 07:07:40 +0900

    Machine Learning

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,6 +5,7 @@ API configuration for stock_prediction project
 from django.urls import path
 from accounts import views as UserViews
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import StockPredictionAPIView
 
 urlpatterns = [
    
@@ -14,6 +15,9 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('protected-view', UserViews.ProtectedView.as_view()),
+
+    # Prediction API
+    path('predict/', StockPredictionAPIView.as_view(), name='stock_prediction')
 
 ]
 
--- backend/stock_prediction/urls.py
+++ backend/stock_prediction/urls.py
@@ -3,10 +3,12 @@ URL configuration for stock_prediction project
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     #BASE API Endpoint
     path('api/v1/', include('api.urls'))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
